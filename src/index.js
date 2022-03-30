289510ceea2bd0e71fc0c1962aabf3949372a932
Juan Moreno <morjua19@gmail.com>
2022-03-30 01:52:14 -0300

    [UPD]

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(
+// react 18
+const root = createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App/>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
+
+// react 17
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
